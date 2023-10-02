c6a8787b716f993e35b48ea011c5af4e3f12da5e
Kavita <thetekavita77@gmail.com>
2023-10-03 01:45:32 +0530

    2nd commit

--- Os_VirtualLab_Backend-main/routes/authRoutes.js
+++ Os_VirtualLab_Backend-main/routes/authRoutes.js
@@ -22,6 +22,19 @@ router.post("/forgot-Password", forgotPasswordController);
 
 //Email verification route
 router.get("/verify/:token", verificationController); 
+// router.get("/trigger-verification/:token", async (req, res) => {
+//   try {
+//     const { token } = req.params;
+//     await verificationController(req, res); // Call the verificationController function
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).send({
+//       success: false,
+//       message: "Error triggering verification",
+//       error,
+//     });
+//   }
+// });
 
 export default router;
 
--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import Login from "./Login";
 import AdminPanel from "./AdminPanel";
 import ForgotPassword from "./ForgotPassword";
 import UserProfile from "./UserProfile";
+import EmailVerify from './EmailVerify';
 
 function App() {
   return(
@@ -16,6 +17,7 @@ function App() {
         <Route path='/register' element={<Register />} />
         <Route path='/AdminPanel/*' element={<AdminPanel />} />
         <Route path='/AdminPanel/user-profile' element={<UserProfile />} />
+        <Route path='/verify/:token' element={<EmailVerify />} />
       </Routes>
     </BrowserRouter>
   );
